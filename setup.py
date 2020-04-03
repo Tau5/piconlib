01d6c17514bfbac6661b5acfd83ab4560edc8187
Tau5 <tau0@tutanota.com>
2020-04-03 15:42:12 +0000

    Identity change?

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="piconlib-Tau5", # Replace with your own username
+    version="0.0.1",
+    author="Tau5",
+    author_email="tau0@tutanota.com",
+    description="A library to interact with Picon's system tree",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tau5/piconlib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Arduino"
+    ],
+    python_requires='>=3.6',
+)
---
+++ testlib.py
@@ -0,0 +1,4 @@
+import piconlib
+
+tree = piconlib.SystemTree("../picon-tree")
+tree.refresh_apploader();
